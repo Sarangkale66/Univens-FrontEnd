da4771c216271a51b69bce51c27b85afa1817768
vishakha omar <omarvishakha@gmail.com>
2025-01-31 12:13:59 +0530

    add

--- src/components/Bgeffectimg.jsx
+++ src/components/Bgeffectimg.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 const Bgeffect = () => {
   return (
     <div className="flex justify-center">
-      <img className="h-[30vh] w-[50vw] opacity-[40%] object-fill top-0 bg-cover filter brightness-30 contrast-125  " src="../public/img2.jpg" style={{ position: "absolute", zIndex: -10 }} alt=""/>
+      <img className="h-[30vh] w-[50vw] opacity-[40%] object-fill top-0 bg-cover filter brightness-30 contrast-125  " src="../public/img3.avif" style={{ position: "absolute", zIndex: -10 }} alt=""/>
     </div>
   );
 };
