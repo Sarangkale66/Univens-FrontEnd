ac11ab0667ddcff095e3d52c6a03269914aa1dbd
vishakha omar <omarvishakha@gmail.com>
2025-02-11 10:06:32 +0530

    herosectionCentered

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,8 +4,8 @@ const HeroSection = () => {
   return (
     
     <div className="text-center py-2 pb-10 px-10 flex-grow flex flex-col mt-16 items-center bg-cover bg-center h-screen justify-center gap-8" >
-      <button className="mt-4 text-[#dbdbe2] text-base md:text-lg z-[10000] bg-[#ffffff23] rounded-3xl px-5 py-1.5 cursor-default" >Business simplified</button>
-      <h2 className="text-2xl md:text-4xl leading-relaxed mt-4 z-[10000] tracking-[2px] ">
+      <button className="mt-4 text-[#dbdbe2] text-base md:text-lg z-[10000] bg-[#ffffff23] rounded-3xl px-5 py-1.5 cursor-default border border-white" >Business simplified</button>
+      <h2 className="text-2xl md:text-4xl leading-relaxed mt-4 z-[10000] tracking-[2px]" style={{ fontFamily: 'Montserrat, sans-serif' }}>
         Grow your business with trusted solutions from reliable experts.
       </h2>
       <div className="h-[40vh] w-[50vw] z-[10000] ">
